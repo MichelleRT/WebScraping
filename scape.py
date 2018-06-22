eece6b7faa7c52a43b9f994797d5a5f8b30a1545
Michy <michytsemichy@gmail.com>
2018-06-22 09:39:43 -0700

    Desciptive comment
    The purpose of the program/more explanation.

--- scape.py
+++ scape.py
@@ -1,1 +1,12 @@
+"""
 
+The point of this program is to web scrape data into text files for training features/weights 
+in BioCompoundML. However, right now, we are keeping the web scraping and training separate, 
+hence we are keeping the web scraping and training programs in different repositories.
+
+Since no other sufficient databases for fuels containing the fuel properties RON (research 
+octane number), TSI (threshold soot index), and MP (melting point) can be found, we are only 
+taking data from the fuel engine co-op database 
+(url: https://fuelsdb.nrel.gov/fmi/webd/FuelEngineCoOptimization).    
+
+"""
