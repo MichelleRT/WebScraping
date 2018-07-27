a659c93f8ac95a08ec2edbdc9ffda4dec885a80d
Tse <michytsemichy@gmail.com>
2018-07-27 10:42:07 -0700

    Webscape successful

--- datamine.py
+++ datamine.py
@@ -7,10 +7,18 @@ Webscrape from web pages.
 from bs4 import BeautifulSoup
 import re
 import urllib2
-import urllib
+import io 
 
 X_file = "http://www.enggcyclopedia.com/2011/01/octane-number/" 
 page = urllib2.urlopen(X_file) 
 soup = BeautifulSoup(page, "html.parser") 
 
-for data in soup.find_all('tr')   
+# Open a new file for storing all the data 
+fileout = io.open("EnggCycData.txt", encoding='utf-8', mode='a') 
+
+for tags in soup.find_all('tr'):
+    data = tags.text.strip()    # strip the tags away 
+    fileout.write(data + "\n")    
+
+# Close the file
+fileout.close() 
