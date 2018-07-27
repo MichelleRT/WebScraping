e0e85a2959789291d023af180969fd59a1383636
Tse <michytsemichy@gmail.com>
2018-07-27 09:45:00 -0700

    Deleted useless files

--- datamine.py
+++ datamine.py
@@ -1,4 +1,16 @@
+"""
+
+Webscrape from web pages. 
+
+"""
+
 from bs4 import BeautifulSoup
 import re
 import urllib2
 import urllib
+
+X_file = "http://www.enggcyclopedia.com/2011/01/octane-number/" 
+page = urllib2.urlopen(X_file) 
+soup = BeautifulSoup(page, "html.parser") 
+
+for data in soup.find_all('tr')   
